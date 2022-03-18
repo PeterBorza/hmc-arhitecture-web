f39230cba82b4968254d1e77e0693af17ef56640
Borza Peter <peter.borza@anaplan.com>
2022-03-18 18:05:13 +0200

    0007 - server and client

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import { LinkContextProvider } from "./context";
 
 import styles from "./App.module.scss";
